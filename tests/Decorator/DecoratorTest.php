1eb17ecb1b6a46d0a28df8cc87432f8b92f7b6b2
Si Thu San <sithusan@aceplussolutions.com>
2022-07-11 21:54:12 +0630

    Change Date on Decorator Test

--- tests/Decorator/DecoratorTest.php
+++ tests/Decorator/DecoratorTest.php
@@ -9,6 +9,13 @@ use SiThuSan\Structural\Decorator\TimeDecorator;
 
 final class DecoratorTest extends TestCase
 {
+    private string $date;
+
+    protected function setUp(): void
+    {
+        $this->date = \date('Y-m-d');
+    }
+
     public function testCanLogWithHtmlDecorator(): void
     {
         $logger  = new HtmlDecorator(new BasicLogger());
@@ -18,12 +25,12 @@ final class DecoratorTest extends TestCase
     public function testCanLogWithTimeDecorator(): void
     {
         $logger = new TimeDecorator(new BasicLogger());
-        $this->assertSame("2022-07-09 Logging", $logger->log("Logging"));
+        $this->assertSame("{$this->date} Logging", $logger->log("Logging"));
     }
 
     public function testCanLogWithHtmlAndTimeDecorator(): void
     {
         $logger = new HtmlDecorator(new TimeDecorator(new BasicLogger()));
-        $this->assertSame("<html>2022-07-09 Logging</html>", $logger->log("Logging"));
+        $this->assertSame("<html>{$this->date} Logging</html>", $logger->log("Logging"));
     }
 }
